69d682eda35c9a464bbffa1642b0d1084f328199
Prabhakar Kumar <prabhakk@mathworks.com>
2025-09-25 20:13:40 +0530

    Remove python code to start MATLAB

--- resources/matlab_launcher.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2020 The MathWorks, Inc.
-
-from os import environ
-import subprocess
-if "MLM_LICENSE_FILE" in environ:
-  subprocess.check_call("matlab")
-else :
-  subprocess.check_call(["matlab", "-desktop", "-licmode", "online"])
